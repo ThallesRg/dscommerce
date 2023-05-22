ca08f14676d05a28c0e9be3b3aa44eac253a7a44
thalles guimaraes <thalles.guimaraes@usp.br>
2023-05-22 08:24:47 -0300

    GET Product by id

---
+++ src/main/java/com/trgthalles/dscommerce/services/ProductService.java
@@ -0,0 +1,24 @@
+package com.trgthalles.dscommerce.services;
+
+import com.trgthalles.dscommerce.dto.ProductDTO;
+import com.trgthalles.dscommerce.entities.Product;
+import com.trgthalles.dscommerce.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    ProductRepository repository;
+    @Transactional(readOnly = true)
+    public ProductDTO findById(Long id){
+        Optional<Product> result = repository.findById(id);
+        Product product = result.get();
+        ProductDTO dto = new ProductDTO(product);
+        return dto;
+    }
+}
